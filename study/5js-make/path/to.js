0ba7dd83134d036b3189e41695b654690a526e92
alxkon <alxkonon@gmail.com>
2016-08-15 17:04:43 +0300

    edit link/javascript

--- study/5js-make/path/to.js
+++
@@ -1,9 +0,0 @@
-alert( 'Проба пера, Добро пожаловать!' );
-
-var years = prompt('Сколько вам лет?', '');
-
-var isYears = confirm('Вам правда ' + years + ' лет?');
-alert( isYears );
-
-var one = 1, two = 2;
-console.log(one + two);
