5f681d458beb76c4d44effb0b6fcd5036dcbca0e
alxkon <alxkonon@gmail.com>
2016-08-15 16:53:31 +0300

    link study

---
+++ study/5js-make/path/to.js
@@ -0,0 +1,9 @@
+alert( 'Проба пера, Добро пожаловать!' );
+
+var years = prompt('Сколько вам лет?', '');
+
+var isYears = confirm('Вам правда ' + years + ' лет?');
+alert( isYears );
+
+var one = 1, two = 2;
+console.log(one + two);
