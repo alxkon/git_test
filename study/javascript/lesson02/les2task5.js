6f24ca1c200918be2602ee1f0d3b9f8f9ee63fbc
alxkon <alxkonon@gmail.com>
2016-08-27 17:11:19 +0300

    Edit folder lesson03 and lesson02

---
+++ study/javascript/lesson02/les2task5.js
@@ -0,0 +1,13 @@
+var name = prompt("Who's there?", "");
+
+(name == 'admin') ? alert('Hi'):
+(name == 'manager') ? alert('Hello'):
+(name == '') ? alert('No login'):
+alert('Who are you?');
+
+var name = 'admin', text;
+
+(name == 'admin') ? text = 'Hi' :
+(name == 'manager') ? text = 'Hello' :
+(name == '') ? text = 'No login' :
+text = '';
---
+++ study/javascript/lesson02/les2task6codewars.js
@@ -0,0 +1,6 @@
+numberToString(num);
+return num + ' ';
+
+
+
+
